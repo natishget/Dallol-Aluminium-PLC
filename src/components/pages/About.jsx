08313c80d2d4bb1f6939b8783fcb521384f5eb65
netnaho <87896136+netnaho@users.noreply.github.com>
2024-05-11 18:25:29 +0300

    started with the about page

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
     <div>
-      <h1>About Page in Development</h1>
+      <h1>About Page</h1>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
