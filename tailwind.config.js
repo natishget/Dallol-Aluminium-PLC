22c7f8fa4a14f1f7d0e6c720200e61e8bf0e6f23
netnaho <87896136+netnaho@users.noreply.github.com>
2024-05-20 16:08:19 +0300

    accentrity ui styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,35 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
-        transparent: 'transparent',
-        current: 'currentColor',
-        'logoC': '#F4FF00',
-        'glass': 'rgba(255, 255, 255, .1)'
+        transparent: "transparent",
+        current: "currentColor",
+        logoC: "#F4FF00",
+        glass: "rgba(255, 255, 255, .1)",
       },
     },
   },
-  plugins: [],
-}
+  plugins: [addVariablesForColors],
+};
 
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(function ([key, val]) {
+      return [`--${key}`, val];
+    })
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
