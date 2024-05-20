0aa11b69236fbfa5d5eb57c806ad1b3bfb97e1d0
kuki44 <ekramsiraj32@gmail.com>
2024-05-20 18:02:04 +0300

    del

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='w-screen bg-slate-500 h-lvh bg-cover'>
+    <div className='w-screen bg-slate-500 h-gi bg-cover'>
       <h1 className=''>Home Page in Development</h1>
       <div ></div>
     </div>
