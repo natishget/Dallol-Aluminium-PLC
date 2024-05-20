cbd41ab9b321e30a0da06286442b9515bb5a1136
natishget <natishget@gmail.com>
2024-05-20 18:27:42 +0300

    sixth

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,53 +16,48 @@ const Footer = () => {
 
       {/* first div */}
       <div className='text-xl  font-bold flex w-full justify-evenly items-center py-8 border-b border-quaternary rounded-full'>
-        <div className='text-quaternary'>Get in touch</div>
+        <div className='text-quaternary hidden md:block'>Get in touch</div>
 
-        <div className='text-5xl font-bold font text-quinary'>Dallol</div>
+        <div className='md:text-5xl text-4xl font-bold font text-quinary'>Dallol</div>
 
         <div className='flex'>
 
-          <div className='w-10 h-10 border border-secondary_t rounded-full flex justify-center items-center mr-5 bg-secondary_t'>
+          <div className='md:w-10 md:h-10 w-8 h-8 border border-secondary_t rounded-full flex justify-center items-center mr-5 bg-secondary_t'>
             <img src={facebook} className=' w-5 h-5' alt="" />
           </div>
-          <div className='w-10 h-10 border border-secondary_t rounded-full flex justify-center items-center mr-5 bg-secondary_t'>
+          <div className='md:w-10 md:h-10 w-8 h-8 border border-secondary_t rounded-full flex justify-center items-center mr-5 bg-secondary_t'>
             <img src={linkedin} alt="" className='w-5 h-5' />
           </div>
-          <div className='w-10 h-10 border border-secondary_t rounded-full flex justify-center items-center bg-secondary_t'>
+          <div className='md:w-10 md:h-10 w-8 h-8 border border-secondary_t rounded-full flex justify-center items-center bg-secondary_t'>
             <img src={telegram} alt="" className='w-5 h-5' />
           </div>
         </div>
       </div>
       {/* second div */}
-      <div className='flex-col  w-full   py-8 text-quaternary font-bold px-10'>
-        <div className='flex w-full'>
-          <div className='w-1/3 flex justify-center'><img src={email} alt="" className='w-6 pb-1' /></div>
-          <div className='w-1/3 flex justify-center'><img src={phone} alt="" className='w-5 pb-1' /> </div>
-          <div className='w-1/3 flex justify-center'><img src={location} alt="" className='w-5 pb-1' /></div>
-        </div>
-        <div className='flex '>
-          <div className='flex-col w-1/3 justify-center items-center '>
+      <div className='w-full md:flex justify-center items-center py-8 text-quaternary font-bold px-10'>
+          <div className='flex-col md:w-1/3 w-full justify-center items-center '>
+            <img src={email} alt="" className='w-6 pb-1' />
             <p className='text-primary'>MAIL</p> 
-            <a href='' className='text-sm'>dallolaluminium@gmail.com</a>
+            <a href='' className='md:text-sm'>dallolaluminium@gmail.com</a>
           </div>
 
-          <div className='w-1/3 flex-col  justify-center items-center'>   
+          <div className='md:w-1/3 flex-col w-full  justify-center items-center mt-5'>   
+            <img src={phone} alt="" className='w-5 pb-1' /> 
             <p className='text-primary'>CALL</p>
-            <a href='' className='text-sm'>+251 911 82 32 71</a><br />
-            <a href='' className='text-sm'>+251 903 23 24 25</a>
+            <a href='' className='md:text-sm'>+251 911 82 32 71</a><br />
+            <a href='' className='md:text-sm'>+251 903 23 24 25</a>
           </div>
 
-          <div className='w-1/3 flex-col justify-center items-center  '> 
+          <div className='md:w-1/3 flex-col w-full justify-center items-center mt-5'> 
+            <img src={location} alt="" className='w-5 pb-1' />
             <p className='text-primary'>FIND US</p>
-            <p className='text-sm'>22 Town Square Mall 1st Floor</p>
-            <p className='text-sm'>Megenagna to Signal Upper Nile - <br /> Real estate 1st Floor </p>  
+            <p className='md:text-sm'>22 Town Square Mall 1st Floor</p>
+            <p className='md:text-sm'>Megenagna to Signal Upper Nile - <br /> Real estate 1st Floor </p>  
           </div>
-
-        </div>
 
       </div>
       {/* third div */}
-      <div className='flex justify-center py-8 border-t rounded-full border-quaternary text-quaternary text-sm'>
+      <div className='flex justify-center py-8 border-t rounded-full border-quaternary text-quaternary text-sm  mt-2 md:mt-0'>
         <p> &copy; 2024 Dallol Aluminum All rights Reserved</p>
       </div>
     </div>
