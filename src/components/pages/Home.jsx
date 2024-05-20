45fc82310832aefff4434804b2816aaf6f8d918e
kuki44 <ekramsiraj32@gmail.com>
2024-05-20 23:12:47 +0300

    hero1

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='w-screen bg-slate-500 h-gi bg-cover'>
-      <h1 className=''>Home Page in Development</h1>
+    <div className='w-screen h-gi'>
+
+      <div className='p-26 pt-32 text-gray-600 flex w-screen justify-around text-sans pb-32'> 
+        <p className='text-6xl font-sans font-bold'>Dallol Alluminim</p>
+        <img src="src/img/one.png" alt="" />
+      </div>
+    
       <div ></div>
     </div>
   )
