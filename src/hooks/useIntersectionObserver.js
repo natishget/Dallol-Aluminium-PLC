1c032e25718fae55cbafda61989952baecc150a2
natishget <natishget@gmail.com>
2024-05-27 12:29:47 +0300

    updated the new contact page

---
+++ src/hooks/useIntersectionObserver.js
@@ -0,0 +1,27 @@
+// src/hooks/useIntersectionObserver.js
+import { useEffect, useRef, useState } from 'react';
+
+const useIntersectionObserver = (options) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const elementRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      setIsVisible(entry.isIntersecting);
+    }, options);
+
+    if (elementRef.current) {
+      observer.observe(elementRef.current);
+    }
+
+    return () => {
+      if (elementRef.current) {
+        observer.unobserve(elementRef.current);
+      }
+    };
+  }, [options]);
+
+  return [elementRef, isVisible];
+};
+
+export default useIntersectionObserver;
